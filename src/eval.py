dab8dd11e75e0ecc7f1cc854cf8c78dce01b4d06
Vitalii Bursov <vitaly@bursov.com>
2019-03-08 19:10:38 +0200

    log what created stack frame, add factorial example using macros

--- src/eval.py
+++ src/eval.py
@@ -29,7 +29,8 @@ class EvalRuntimeError(Exception):
     pass
 
 class EvalQuoted(object):
-    pass
+    start = None
+    end = None
 
 Rest = type("Rest", (EvalQuoted,), {})()
 Begin = type("Begin", (EvalQuoted,), {})()
@@ -50,10 +51,18 @@ class Macro(object):
 
 
 class Frame(object):
-    def __init__(self, env, data, cont):
+    def __init__(self, env, data, cont, descr="", src=None):
+        self.descr = ""
         self.env = env
         self.data = data
         self.cont = cont
+
+        if src and src.start:
+            self.descr += "{:3d}:{:2d} ".format(src.start.line, src.start.column)
+        else:
+            self.descr += "       "
+        if descr:
+            self.descr += descr
 
     def __repr__(self):
         return repr(self.data)
@@ -100,7 +109,7 @@ class VMEval(object):
         if self.trace_size <= 0:
             return
 
-        if hasattr(exp, 'start') and exp.start != 0:
+        if hasattr(exp, 'start') and exp.start:
                 l = exp.start.buffer.splitlines()
                 ln = exp.start.line
                 cn = exp.start.column
@@ -175,8 +184,8 @@ eval1 = VMEval(trace=10)
 
 def eval2(vm, stack, env, exp):
 
-    def s_push(env, cont, data):
-        stack.append(Frame(env, data, cont))
+    def s_push(env, cont, data, descr=None, src=None):
+        stack.append(Frame(env, data, cont, descr=descr, src=src))
 
     def s_reset(st):
         while stack:
@@ -191,7 +200,7 @@ def eval2(vm, stack, env, exp):
     def op_list(env, exp):
         if len(exp.v) == 0:
             return (VList([]),)
-        s_push(env, op_list2, (exp, []))
+        s_push(env, op_list2, (exp, []), descr="List:1", src=exp)
         return exp.v[0]
 
     def op_list2(env, exp, _cexp, cdata, frame):
@@ -200,13 +209,13 @@ def eval2(vm, stack, env, exp):
         if len(acc) >= len(cdata.v):
             return (VList(acc),)
 
-        s_push(env, op_list2, (cdata, acc))
+        s_push(env, op_list2, (cdata, acc), descr="List:N", src=exp)
         return cdata.v[len(acc)]
 
 
     def op_pylist(env, exp):
         if len(exp) > 1:
-            s_push(env, op_pylist2, (exp, []))
+            s_push(env, op_pylist2, (exp, []), descr="pylist:1", src=exp)
 
         env.weaken()
         return exp[0]
@@ -219,7 +228,7 @@ def eval2(vm, stack, env, exp):
             env.weaken()
             return cdata[len(acc)]
 
-        s_push(env, op_pylist2, (cdata, acc))
+        s_push(env, op_pylist2, (cdata, acc), descr="pylist:N", src=exp)
         return cdata[len(acc)]
 
     def callcc(envn):
@@ -258,7 +267,7 @@ def eval2(vm, stack, env, exp):
         return Apply(0,0, exp, [])
 
     def op_apply(env, exp):
-        s_push(env, op_apply2, exp)
+        s_push(env, op_apply2, exp, descr="apply:E", src=exp)
         return exp.sym
 
     def op_apply2(env, f, _cexp, cdata, frame):
@@ -269,33 +278,33 @@ def eval2(vm, stack, env, exp):
         if f is EIf:
             if len(cdata.args) not in (2, 3):
                 raise EvalRuntimeError("&if requires 2 or 3 arguments")
-            s_push(env, op_eif2, cdata.args[1:])
+            s_push(env, op_eif2, cdata.args[1:], descr="apply:if", src=cdata)
             return cdata.args[0]
 
         if f is Define:
             if len(cdata.args) < 2:
                 raise EvalRuntimeError("define requires 2 arguments or more")
-            s_push(env, op_define, cdata.args[0])
+            s_push(env, op_define, cdata.args[0], descr="apply:def", src=cdata)
             return cdata.args[1:]
 
         if f is DefMacro:
             if len(cdata.args) < 2:
                 raise EvalRuntimeError("macro requires 2 arguments - symbol and lambda")
-            s_push(env, op_macro, cdata.args[0])
+            s_push(env, op_macro, cdata.args[0], descr="apply:mac", src=cdata)
             return cdata.args[1:]
 
         if f is CallMacro:
             if len(cdata.args) != 2:
                 raise EvalRuntimeError("callmacro requires 2 arguments - symbol and args list")
 
-            s_push(env, op_callmacro, cdata.args[0])
+            s_push(env, op_callmacro, cdata.args[0], descr="apply:mc", src=cdata)
             return cdata.args[1]
 
         if f is Eval:
             if len(cdata.args) != 1:
                 raise EvalRuntimeError("eval requires 1 argument of list()")
 
-            s_push(env, op_eval, None)
+            s_push(env, op_eval, None, descr="apply:eval", src=cdata)
             return cdata.args
 
         if f is Quote:
@@ -334,7 +343,7 @@ def eval2(vm, stack, env, exp):
         ctx = (f, args, argv)
         largs = cdata.args
 
-        s_push(env, op_apply3, ctx)
+        s_push(env, op_apply3, ctx, descr="apply:A", src=cdata)
         if isinstance(largs, list):
             return EList(largs)
 
@@ -383,10 +392,10 @@ def eval2(vm, stack, env, exp):
 
 #            print(exp)
 
-            if len(stack) > 20:
+            if len(stack) > 100:
                 print("my stack size: {}".format(len(stack)))
                 for x in stack:
-                    print(x)
+                    print("{:12s} - {}".format(x.descr, x))
                 1/0
             vm.stats.stack(len(stack))
 
